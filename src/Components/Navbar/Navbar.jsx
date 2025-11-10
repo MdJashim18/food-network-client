8c16de5e0726eb1e5ba344ebeedae799d9049064
MdJashim18 <mdjashimuddinjnn22990@gmail.com>
2025-11-10 23:54:03 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    const links = <>
+        <li><NavLink to='/'>Home</NavLink> </li>
+        <li><NavLink to='/allFoods'>All Foods</NavLink> </li>
+        {
+            // user && <>
+            //     <li><NavLink to='/myProducts'>My Products</NavLink> </li>
+            //     <li><NavLink to='/myBids'>Bids</NavLink> </li>
+            //     <li><NavLink to='/createAProduct'>Create A Product</NavLink> </li>
+            // </>
+        }
+    </>
+
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> 
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                       {links}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Food Lovers</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {/* User Profile Image - Fully Round */}
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img 
+                                alt="User Profile" 
+                                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=100&q=80" 
+                            />
+                        </div>
+                    </div>
+                    <ul 
+                        tabIndex={0} 
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                    >
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer/Footer';
+
+const RootLayout = () => {
+    return (
+        <div className='w-7xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
